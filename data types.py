eb87f3fadc7e6c004f12c3bfe7e13255af6b6739
Najafsyed <156337418+Najafsyed@users.noreply.github.com>
2024-01-16 22:03:54 +0500

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,15 @@
+flot_num=5.7
+print(flot_num,"is of type:",type(flot_num))
+
+#string
+string="hello world"
+print(flot_num,"is of type:",type(flot_num))
+#string
+#boolean
+boolean=(5==5)
+print(boolean,"is of type:",type boolean))
+#list
+list=[1,2,,3,4]
+print(list,"is of type:",type_list))
+#set
+set={}
---
+++ task1.py
@@ -0,0 +1,7 @@
+#tast 1
+
+#sum of expression are
+sum=int(1+2+3+4+5)
+#printing result 
+print("the sum of expression is",sum)
+
